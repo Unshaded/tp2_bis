3942a059d4d3403da24e0ac9990f468eebe13661
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-05-04 04:08:54 -0400

    debut

---
+++ chifoumi.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Chifoumi</title>
+  </head>
+  <body>
+    <legend>
+      Pierre,Feuille,Ciseau
+    </legend>
+    <p>
+      Choisissez votre coup:
+      <a href="coup=0"><img src="pierre.png" width="50"></a>
+      <a href="coup=1"><img src="feuille.png" width="50"></a>
+      <a href="coup=2"><img src="pierre.png" width="50"></a>
+    </p>
+    <h3>
+      Votre Coup ______________________________________ Celui de l'ordinateur
+    </h3>
+    <?php
+      extract($_GET);
+      if(isset($coup)){
+      if($coup=='0'){
+        echo"<img src='pierre.png' width='150'>";
+      }
+      if($coup=='1'){
+        echo"<img src='feuille.png' width='150'>";
+           }
+      if($coup=='2'){
+        echo"<img src='pierre.png' width='150'>";
+      }
+       }
+    ?>
+    
+  </body>
+</html>
